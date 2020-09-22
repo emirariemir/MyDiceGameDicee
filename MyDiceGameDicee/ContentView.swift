fcad637e7ed2513fc89098201eae7be87d2ffd81
Emir Ari <emirariomatic@gmail.com>
2020-09-22 17:06:18 +0300

    Layout done without any functionality

--- MyDiceGameDicee/ContentView.swift
+++ MyDiceGameDicee/ContentView.swift
@@ -9,13 +9,56 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        ZStack {
+            Image("background")
+                .resizable()
+                .ignoresSafeArea()
+            
+            VStack {
+                Image("diceeLogo").padding(.vertical)
+                
+                Spacer()
+                
+                HStack {
+                    DiceView()
+                    DiceView()
+                }.padding(.horizontal)
+                
+                Spacer()
+                
+                ButtonView()
+            }
+        }
+        
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct DiceView: View {
+    var body: some View {
+        Image("dice1")
+            .resizable()
+            .aspectRatio(1, contentMode: .fit)
+            .padding(.horizontal)
+    }
+}
+
+struct ButtonView: View {
+    var body: some View {
+        Button(action: {
+            
+        }, label: {
+            Text("Roll")
+                .font(.system(size: 50))
+                .fontWeight(.heavy)
+                .foregroundColor(.white)
+                .padding(.horizontal)
+        }).background(Color.red)
     }
 }
